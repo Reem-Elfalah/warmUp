79bd308792bbd2c963ec11ffe9e9bb973671073b
ReemElfalah <r.fellah@yahoo.com>
2020-03-23 09:45:17 +0100

    warmUp 9 solved!

--- warmUp9.js
+++ warmUp9.js
@@ -4,6 +4,10 @@
 //1. Write a JavaScript function to check whether an `input` is an array or not
 // ex isArray([1,2])=>true
 //    isArray(5)=>false
+//check whether the input is array ot not not using array methods.
+function isArray(input){
+	return Array.isArray(input);
+}
 
 // **************** git status ****************
 // **************** git add fileName.js ****************
@@ -15,6 +19,18 @@
 //2-Write a JavaScript function to get the first n element of an array.
 //ex  first([1,2,3],1)=>[1]
 //    first([1,2,3,4]2)=>[1,2]
+// a loop is needed to find the required number of elements, same n given by the
+// user will be used but after decreasing 1, so it matches the current element required in the array since arrays
+// start with index 0, unshift is used to place the elements in order.
+function first(array,n){
+	let arr = [];
+	for(n--; n >= 0; n--){
+		arr.unshift(array[n])
+	}
+
+	return arr
+}
+
 
 // **************** git status ****************
 // **************** git add fileName.js ****************
@@ -25,6 +41,17 @@
 
 //3- Write a JavaScript function to convert an object into a list of `[key, value]` pairs
 //ex: convert({name:'ahmed',age:20})=>[['name','ahmed'],['age', 20]]
+ //first keys must be defined, so I used the object method keys. and then a loop is required to get each value for those keys
+ // and place it next to its key in the final array.
+
+function convert(obj){
+	let arrayOfKeys = Object.keys(obj); 
+	let array = [];
+	for(let i = 0; i < arrayOfKeys.length; i++){
+		array[i] = [arrayOfKeys[i], obj[arrayOfKeys[i]]];
+		}
+return array
+}
 
 // **************** git status ****************
 // **************** git add fileName.js ****************
